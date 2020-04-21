f5e8b1241252dd9bef67cf9bc69b2e4201f97d49
crazyzlj <crazyzlj@gmail.com>
2020-04-21 14:06:02 +0800

    delete previous repo

--- scripts/calibration/__init__.py
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/env python
-# -*- coding: utf-8 -*-
-"""
-  Calibrate SWAT by NSGA-II based on DEAP and SCOOP (for parallel computation on cluster).
-"""
--- swat_src/readme.md
+++
@@ -1,5 +0,0 @@
-SWAT source distributed website: http://swat.tamu.edu/software/swat-executables/
-
-Changlog:
-
-+ 12/23/2016: rev.664
